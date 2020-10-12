cb57deadd77d3eb64a37d7ff963985145aae7279
Jeff Tsai <tsai81926@gmail.com>
2020-10-13 02:30:27 +0800

    add US

--- driving.py
+++ driving.py
@@ -1,9 +1,13 @@
 country = input('請輸入您的國家: ')
 age = input('請輸入您的年齡: ')
 age = int(age)
-if country == '臺灣' or 'TW' or '中華民國':
+if country == '臺灣':
 	if age >= 18:
 		print('您可以考駕照了!')
-	else :
+	else:
 		print('您還不能考駕照!')
-		
+elif country == '美國':
+	if age >= 16:
+		print('You can drive!')
+	else:
+		print('You can not drive!')
